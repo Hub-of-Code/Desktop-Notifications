11e27cda3de06c93c8ca1023cd6c344ff072e6aa
Edinyanga O. Ottoho <elzucky@gmail.com>
2019-01-31 11:16:04 +0100

    Add files via upload

---
+++ receive.php
@@ -0,0 +1,5 @@
+<?php
+  if (isset($_GET['var'])){
+  	 echo "Edinyanga Ottoho Shared a post&&&The rate at which Africans are going, they will get extinct|||Emmanuel Haankwenda Shared a Post&&&I love Javascript so much and I will like to develop a social media platform with it";
+  }
+?> 
